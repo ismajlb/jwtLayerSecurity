de1a26e4479157f93c0ca645731738cc260732c5
Ismajlb <ismajl.bina@gmail.com>
2024-04-10 00:56:31 +0200

    Adding in the github

---
+++ src/main/java/com/jwt/jwtapisecuring/JwtapisecuringApplication.java
@@ -0,0 +1,13 @@
+package com.jwt.jwtapisecuring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtapisecuringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtapisecuringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jwt/jwtapisecuring/JwtapisecuringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwt.jwtapisecuring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtapisecuringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
